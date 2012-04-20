21fa9428073293b946e64a95fd8c8a87b82355c0
Clemens Prerovsky <c.prerovsky@gentics.com>
2012-04-20 17:14:43 +0200

    overlay of different items works fine

--- style-o-matic/gen/at/docstoegprer/android/R.java
+++ style-o-matic/gen/at/docstoegprer/android/R.java
@@ -11,36 +11,45 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int sample_0=0x7f020001;
-        public static final int sample_1=0x7f020002;
-        public static final int sample_2=0x7f020003;
-        public static final int sample_3=0x7f020004;
-        public static final int sample_4=0x7f020005;
-        public static final int sample_5=0x7f020006;
-        public static final int sample_6=0x7f020007;
-        public static final int sample_7=0x7f020008;
-        public static final int top=0x7f020009;
+        public static final int accessory=0x7f020000;
+        public static final int bottom=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int sample_0=0x7f020003;
+        public static final int sample_1=0x7f020004;
+        public static final int sample_2=0x7f020005;
+        public static final int sample_3=0x7f020006;
+        public static final int sample_4=0x7f020007;
+        public static final int sample_5=0x7f020008;
+        public static final int sample_6=0x7f020009;
+        public static final int sample_7=0x7f02000a;
+        public static final int shoes=0x7f02000b;
+        public static final int top=0x7f02000c;
     }
     public static final class id {
-        public static final int accessories=0x7f060002;
-        public static final int bottoms=0x7f060004;
-        public static final int button1=0x7f060007;
-        public static final int cam=0x7f060000;
-        public static final int flipper=0x7f060006;
-        public static final int galleryAccessories=0x7f06000c;
-        public static final int galleryBottoms=0x7f060009;
-        public static final int galleryShoes=0x7f06000b;
-        public static final int galleryTops=0x7f060008;
-        public static final int gridviewTops=0x7f06000a;
-        public static final int shoes=0x7f060005;
-        public static final int top=0x7f060001;
-        public static final int tops=0x7f060003;
+        public static final int accessories=0x7f070001;
+        public static final int bottoms=0x7f070003;
+        public static final int button1=0x7f070006;
+        public static final int cam=0x7f070000;
+        public static final int flipper=0x7f070005;
+        public static final int galleryAccessories=0x7f07000b;
+        public static final int galleryBottoms=0x7f070008;
+        public static final int galleryShoes=0x7f07000a;
+        public static final int galleryTops=0x7f070007;
+        public static final int gridviewTops=0x7f070009;
+        public static final int menu_accessories=0x7f07000c;
+        public static final int menu_bottoms=0x7f07000e;
+        public static final int menu_shoes=0x7f07000f;
+        public static final int menu_tops=0x7f07000d;
+        public static final int shoes=0x7f070004;
+        public static final int tops=0x7f070002;
     }
     public static final class layout {
         public static final int camera=0x7f030000;
         public static final int itempicker=0x7f030001;
         public static final int main=0x7f030002;
+    }
+    public static final class menu {
+        public static final int cammenu=0x7f060000;
     }
     public static final class string {
         public static final int accessories=0x7f040002;
