bb81743b3c2caac4ab2f7921cf5f488dc6cc017e
Clemens Prerovsky <c.prerovsky@gentics.com>
2012-04-20 16:18:47 +0200

    cam preview with overlay working

---
+++ style-o-matic/src/at/docstoegprer/android/OutDir.java
@@ -0,0 +1,23 @@
+package at.docstoegprer.android;
+
+public final class OutDir {
+	/**
+	 * output dir for accessories
+	 */
+	public static final String ACCESSORIES = "Style-o-matic/accessories";
+	
+	/**
+	 * output dir for tops
+	 */
+	public static final String TOPS        = "Style-o-matic/tops";
+	
+	/**
+	 * output dir for bottoms
+	 */
+	public static final String BOTTOMS     = "Style-o-matic/bottoms";
+	
+	/**
+	 * output dir for shoes
+	 */
+	public static final String SHOES       = "Style-o-matic/shoes";
+}
